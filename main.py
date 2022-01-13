2838b1de786c36db575b235dee2af7ff6214b390
BrandonZentoLi <91547470+BrandonZentoLi@users.noreply.github.com>
2022-01-12 20:35:50 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # import modules
 import pygame, sys, cv2
+
+# import our own logic from other ifles
 from button import Button
+from text_wrap import *
 
 pygame.init()
 
@@ -11,7 +14,7 @@ screen_width, screen_height = pygame.display.get_surface().get_size()
 
 pygame.display.set_caption("Trials of the Trinity")
 title_font = pygame.font.Font('fonts/pixel.ttf', 32)
-button_font = pygame.font.Font('fonts/pixel.ttf', 15)
+text_font = pygame.font.Font('fonts/pixel.ttf', 15)
 
 text = title_font.render('Trials of the Trinity', True, "#475F77")
 textRect = text.get_rect(center = (screen_width // 2, 100))
@@ -35,17 +38,18 @@ def update_page_id(id):
 
 
 homeButton = Button('Home', 80, 40, (360, 350), 7,
-                    window, button_font, 'home', update_page_id)
+                    window, text_font, 'home', update_page_id)
 instructionsButton = Button('Info', 80, 40, (160, 350), 7,
-                            window, button_font, 'info', update_page_id)
+                            window, text_font, 'info', update_page_id)
 lessonButton = Button('Lesson', 80, 40, (260, 350), 7,
-                      window, button_font, 'lesson', update_page_id)
+                      window, text_font, 'lesson', update_page_id)
 startButton = Button('Start', 80, 40, (360, 350), 7, window,
-                     button_font, 'start', update_page_id)
+                     text_font, 'start', update_page_id)
 quizButton = Button('Quiz', 80, 40, (460, 350), 7, window,
-                    button_font, 'quiz', update_page_id)
+                    text_font, 'quiz', update_page_id)
 quitButton = Button('Quit', 80, 40, (560, 350), 7, window,
-                    button_font, 'quit', update_page_id)
+                    text_font, 'quit', update_page_id)
+
 
 # functions
 
@@ -69,9 +73,11 @@ def show_home():
 
 
 def show_info():
-    text = title_font.render('Info Page', True, "#475F77")
+    text = title_font.render('Info Page', True, '#475F77')
     textRect = text.get_rect(center = (screen_width // 2, 100))
 
+    renderTextCenteredAt('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', text_font, '#475F77', 500, 150, window, screen_width)
+    
     window.blit(text, textRect)
 
     homeButton.draw()
