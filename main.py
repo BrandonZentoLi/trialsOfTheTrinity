01449a1cc2b8d59f205d5f996ff7809c7aced694
BrandonZentoLi <91547470+BrandonZentoLi@users.noreply.github.com>
2022-01-15 18:36:47 -0500

    Added background music for main menu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import modules
-import pygame, sys, cv2
+import pygame, cv2
+from pygame import mixer
 
 # import our own logic from other ifles
 from button import Button
@@ -16,13 +17,18 @@ pygame.display.set_caption("Trials of the Trinity")
 title_font = pygame.font.Font('fonts/pixel.ttf', 32)
 text_font = pygame.font.Font('fonts/pixel.ttf', 15)
 
+# playing music
+mixer.init()
+mixer.music.load('music/celesteMenuMusic.mp3')
+mixer.music.play(-1)
+
 # for the book of insight pages:
 header_font = pygame.font.Font('fonts/pixel.ttf', 20)
 
 text = title_font.render('Trials of the Trinity', True, "#475F77")
 textRect = text.get_rect(center = (screen_width // 2, 100))
 
-capture = cv2.VideoCapture('resources/titlescreen.mp4')
+capture = cv2.VideoCapture('videos/titlescreen.mp4')
 _, image = capture.read()
 shape = image.shape[1::-1]
 
@@ -33,6 +39,8 @@ fps = capture.get(cv2.CAP_PROP_FPS)
 bookOfInsights = pygame.image.load("images/bookofinsights.jpg")
 left_page_x_pos = 250
 left_page_y_pos = 125
+right_page_x_pos = 550
+right_page_y_pos = 125
 book_width = 210
 # titlescreen = pygame.image.load("titlescreen.jpg")
 # titlescreen = pygame.transform.scale(titlescreen, size)
@@ -56,6 +64,8 @@ quizButton = Button('Quiz', 80, 40, (460, 350), 7, window,
                     text_font, 'quiz', update_page_id)
 quitButton = Button('Quit', 80, 40, (560, 350), 7, window,
                     text_font, 'quit', update_page_id)
+nextPageButton = Button('Next >', 80, 40, (700, 400), 7, window, 
+                    text_font, 'next', update_page_id)
 
 
 # functions
@@ -85,10 +95,15 @@ def show_home():
 def show_info():
     renderTextCenteredAt('Info Page', header_font, '#475F77', left_page_x_pos, left_page_y_pos - 40, window, book_width,)
 
-    renderTextCenteredAt("Trials of the Trinity is an interactive story in which you are a mortal that has angered the Big Three of the Olympian gods. Zeus,the God of the Sky. Poseidon, the God of the Sea. Hades, the God of the Underworld. To punish you for what you have done, they've decided to give you a trial, and have teleported you to an island.", text_font, '#475F77', left_page_x_pos, left_page_y_pos, window, book_width,)
+    renderTextCenteredAt("Trials of the Trinity is an interactive story in which you are a mortal that has angered the Big Three of the Olympian gods. Zeus, the God of the Sky - Poseidon, the God of the Sea - and Hades, the God of the Underworld. To punish you for what you have done, they've decided to give you a trial, and have teleported you to an island.", text_font, '#475F77', left_page_x_pos, left_page_y_pos, window, book_width,)
     
+    renderTextCenteredAt("If you want to make it to tomorrow, you must survive the trials given to you by the gods. Pick your choices wisely, for your fate rests in your hands, mortal.",  text_font, '#475F77', right_page_x_pos, right_page_y_pos - 40, window, book_width,)
 
     homeButton.draw()
+    nextPageButton.draw()
+
+def info_page_two():
+  renderTextCenteredAt
 
 
 def show_lesson():
@@ -110,9 +125,8 @@ def show_start():
 
 
 def show_quiz():
-    text = title_font.render('Quiz Page', True, "#475F77")
-    textRect = text.get_rect(center = (screen_width // 2, 100))
-    window.blit(text, textRect)
+    show_book_of_insights()
+    renderTextCenteredAt('Quiz Page', header_font, '#475F77', left_page_x_pos, left_page_y_pos - 40, window, book_width,)
 
     homeButton.draw()
 
@@ -135,6 +149,7 @@ def main():
             show_book_of_insights()
             show_info()
         elif page_id == 'lesson':
+            show_book_of_insights()
             show_lesson()
         elif page_id == 'start':
             show_start()
