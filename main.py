6d0a79dbffd8d413ab92c9d21b965312023d0e69
BrandonZentoLi <91547470+BrandonZentoLi@users.noreply.github.com>
2022-01-15 14:27:44 -0500

    changes to info page, positioning and dynamic code

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ pygame.display.set_caption("Trials of the Trinity")
 title_font = pygame.font.Font('fonts/pixel.ttf', 32)
 text_font = pygame.font.Font('fonts/pixel.ttf', 15)
 
+# for the book of insight pages:
+header_font = pygame.font.Font('fonts/pixel.ttf', 20)
+
 text = title_font.render('Trials of the Trinity', True, "#475F77")
 textRect = text.get_rect(center = (screen_width // 2, 100))
 
@@ -41,8 +44,7 @@ def update_page_id(id):
     global page_id
     page_id = id
 
-
-homeButton = Button('Home', 80, 40, (360, 350), 7,
+homeButton = Button('< Home', 80, 40, (20, 400), 7,
                     window, text_font, 'home', update_page_id)
 instructionsButton = Button('Info', 80, 40, (160, 350), 7,
                             window, text_font, 'info', update_page_id)
@@ -81,8 +83,9 @@ def show_home():
 
 
 def show_info():
-    renderTextCenteredAt('Info Page', text_font, '#475F77', left_page_x_pos, left_page_y_pos - 25, window, book_width,)
-    renderTextCenteredAt('Trials of the Trinity is an interactive story in which you are a mortal that has angered the Big Three of the Olympian gods.', text_font, '#475F77', left_page_x_pos, left_page_y_pos, window, book_width,)
+    renderTextCenteredAt('Info Page', header_font, '#475F77', left_page_x_pos, left_page_y_pos - 40, window, book_width,)
+
+    renderTextCenteredAt("Trials of the Trinity is an interactive story in which you are a mortal that has angered the Big Three of the Olympian gods. Zeus,the God of the Sky. Poseidon, the God of the Sea. Hades, the God of the Underworld. To punish you for what you have done, they've decided to give you a trial, and have teleported you to an island.", text_font, '#475F77', left_page_x_pos, left_page_y_pos, window, book_width,)
     
 
     homeButton.draw()
